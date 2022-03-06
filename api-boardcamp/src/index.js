b5baf3c3f264ec6d93abbf44cd0edd426a052ba9
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2022-03-06 17:52:07 -0300

    start

---
+++ api-boardcamp/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import router from './routes/index.js';
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.use(router);
+
+server.listen(4000, () => {
+  console.log("Listening on 4000")
+})
